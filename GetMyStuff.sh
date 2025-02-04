ed333070133e7b606e56a508c928386d9eb27702
Hannah Beatty <hannahrbeatty@gmail.com>
2025-02-03 23:50:41 -0500

    homework files

---
+++ GetMyStuff.sh
@@ -0,0 +1,11 @@
+
+
+#!/bin/tcsh
+
+echo "User ID: `id -u`"
+echo "Primary Group ID: `id -g`"
+echo "All groups: `id -Gn`"
+echo "Shell Path: $SHELL"
+echo "Home Directory: $HOME"
+echo "PATH Variable: $PATH"
+
